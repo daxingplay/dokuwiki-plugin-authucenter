b374917f1c88795d6be878567bc03f1ed2f018b9
daxingplay <daxingplay@gmail.com>
2016-02-15 11:15:24 +0800

    init

---
+++ conf/default.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Default settings for the authucenter plugin
+ *
+ * @author daxingplay <daxingplay@gmail.com>
+ */
+
+//$conf['fixme']    = 'FIXME';
---
+++ conf/metadata.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Options for the authucenter plugin
+ *
+ * @author daxingplay <daxingplay@gmail.com>
+ */
+
+
+//$meta['fixme'] = array('string');
+
